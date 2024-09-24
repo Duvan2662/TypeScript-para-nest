51d0ecbf3b9b3494ea3c08503642b797b44a4c4a
Duvan <duvanriano2626@gmail.com>
2024-09-24 16:36:01 -0500

    Getter setter y this

--- src/bases/03-clases.ts
+++ src/bases/03-clases.ts
@@ -1,19 +1,43 @@
 export class Pokemon {
     public id :number;
     public name : string;
+    public imagenurl:string;
 
-    constructor(id:number,name:string){
+
+    constructor(id:number,name:string, url:string){
         this.id = id;
-        this.name= name;
+        this.name = name;
+        this.imagenurl = url;
         console.log('Llamado al constructor');
     }
 
     
+    public get getname() : string {
+        return this.name
+    }
+
+    
+    public set setname(value : string) {
+        this.name= value;
+    }
     
     
+    public scream() {
+        console.log(`
+            ${this.name.toUpperCase()} !!!
+            `);
+    }
+
+    
+    public speak() {
+        console.log(`${this.name} , ${this.name}`);
+    }
     
 }
 
-export const charmander = new Pokemon(1,'charmander');
+export const charmander = new Pokemon(1,'charmander','https://pokemon.com/4');
 // charmander.id = 3;
 // charmander.name = 'alura'
+
+charmander.scream();
+charmander.speak();
