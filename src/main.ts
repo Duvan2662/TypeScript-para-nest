dc14a85d12e06705af5474e350ac333ad6835a17
Duvan <duvanriano2626@gmail.com>
2024-09-25 16:26:32 -0500

    Decorador de metodo

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
-import { charmander } from './bases/05-decorators'
+import { charmander } from './bases/06-decorators2'
 import './style.css'
 // import { charmander } from './bases/03-clases.ts'
 // import { name, age } from "./bases/01-types.ts";
 // import { charmander, pokemonIds, pokemons } from './bases/02-objects.ts'
 // import { charmander } from './bases/04-injection.ts'
+// import { charmander } from './bases/05-decorators'
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <h1>Hello ${charmander.name}</h1>
 `
