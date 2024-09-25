a38081d01fc8b947e16180f4341e9e66148d6f8b
Duvan <duvanriano2626@gmail.com>
2024-09-25 15:28:32 -0500

    Colocar tipo de dato a las respuestas https (Genericos)

--- src/bases/03-clases.ts
+++ src/bases/03-clases.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { Move, PokeAPIResponse } from "../interfaces/pokeapi-response.interface";
 
 export class Pokemon {
     public id :number;
@@ -35,13 +36,12 @@ export class Pokemon {
         console.log(`${this.name} , ${this.name}`);
     }
 
-    public async getmoves(){
+    public async getmoves(): Promise <Move[]>{
         // const moves = 0;
-        const {data} = await axios.get('https://pokeapi.co/api/v2/pokemon/4');
+        const {data} = await axios.get<PokeAPIResponse>('https://pokeapi.co/api/v2/pokemon/4');
         console.log(data.moves);
 
         return data.moves;
-        
     }
     
 }
