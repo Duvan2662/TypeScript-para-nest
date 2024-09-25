40a106a3c40c6893effa7c1a696f67c15cb86222
Duvan <duvanriano2626@gmail.com>
2024-09-25 15:18:47 -0500

    Metodos asincronos

--- src/bases/03-clases.ts
+++ src/bases/03-clases.ts
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 export class Pokemon {
     public id :number;
     public name : string;
@@ -32,6 +34,15 @@ export class Pokemon {
     public speak() {
         console.log(`${this.name} , ${this.name}`);
     }
+
+    public async getmoves(){
+        // const moves = 0;
+        const {data} = await axios.get('https://pokeapi.co/api/v2/pokemon/4');
+        console.log(data.moves);
+
+        return data.moves;
+        
+    }
     
 }
 
@@ -39,5 +50,9 @@ export const charmander = new Pokemon(1,'charmander','https://pokemon.com/4');
 // charmander.id = 3;
 // charmander.name = 'alura'
 
-charmander.scream();
-charmander.speak();
+// charmander.scream();
+// charmander.speak();
+
+// console.log(charmander.getmoves());
+
+charmander.getmoves();
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
 import './style.css'
-import { setupCounter } from './counter.ts'
 import { charmander } from './bases/03-clases.ts'
 // import { name, age } from "./bases/01-types.ts";
 // import { charmander, pokemonIds, pokemons } from './bases/02-objects.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <h1>${charmander}</h1>
+  <h1>Hello ${charmander.getname}</h1>
 `
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+
