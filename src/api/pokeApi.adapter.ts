d189e48277186871d6badc44986ac296595aa779
Duvan <duvanriano2626@gmail.com>
2024-09-25 16:07:25 -0500

    Resolver el principio de sustitucion

--- src/api/pokeApi.adapter.ts
+++ src/api/pokeApi.adapter.ts
@@ -1,25 +1,33 @@
 import axios from "axios";
 
 
+export interface HttpAdapter{
+    getRequest<T>( url:string ):Promise<T>;
+}
 
-export class PokeApiFetchAdapter {
+
+
+
+export class PokeApiFetchAdapter implements HttpAdapter {
 
     public async getRequest<T>(url: string): Promise<T> {
         const resp = await fetch(url);
         const data:T = await resp.json();
-
+        console.log("Con Fetch");
         return data;
     }
 }
 
 
-export class PokeApiAdapter {
+export class PokeApiAdapter implements HttpAdapter {
     
     private readonly axios = axios;
 
     public async getRequest<T>(url: string): Promise<T> {
         //peticion get
         const { data } = await this.axios.get<T>(url);
+        console.log("Con axios");
+        
         return data
     }
     public async postRequest(url: string, data:any) {
